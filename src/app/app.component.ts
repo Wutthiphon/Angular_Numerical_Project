9ad25c768c55ca719d9b9af25455d540a274e871
Wutthiphon Main PC <woodychgamer5588@gmail.com>
2023-09-19 20:09:57 +0700

    update css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,30 +17,11 @@ import Swal from 'sweetalert2';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  isApiLoad: boolean = true;
-  isLogin: boolean = false;
-  username: string = '';
   sidebarVisible: boolean = false;
-  profile_items: MenuItem[] = [];
   aside_items: any[] = [];
 
   url_path: string[] = [];
   current_url_path: string[] = [];
-
-  force_refresh: boolean = false;
-
-  have_profile_image: boolean = false;
-  user_profile_image: string =
-    './assets/image/not_found_image/profile_image_not_found.png';
-
-  changepassword_dialog: boolean = false;
-  changepassword_form: any = {
-    old_password: '',
-    new_password: '',
-    confirm_password: '',
-    input_error: '',
-    input_class: '',
-  };
 
   constructor(
     private router: Router,
@@ -53,50 +34,23 @@ export class AppComponent implements OnInit {
     this.router.events.subscribe(async () => {
       this.loadMenu();
     });
-
-    //Load Check Login
-    this.profile_items = [
-      {
-        label: 'ข้อมูลพื้นฐาน',
-        icon: 'pi pi-user',
-        routerLink: '/',
-        command: () => {
-          // this.openPage();
-        },
-      },
-      {
-        label: 'เปลี่ยนรหัสผ่าน',
-        icon: 'pi pi-key',
-        command: () => {
-          // this.ChangePassword();
-        },
-      },
-      {
-        separator: true,
-      },
-      {
-        label: 'ออกจากระบบ',
-        icon: 'pi pi-sign-out',
-        command: () => {
-          // this.logout();
-        },
-      },
-    ];
   }
 
   async loadMenu() {
     this.current_url_path = this.router.url.split('/');
-    let url_change = false;
-    if (this.url_path[2] != this.current_url_path[2]) {
-      this.current_url_path[2] = this.url_path[2];
-      url_change = true;
-    }
+
     // Update Url Path
     this.url_path = this.router.url.split('/');
 
     // Reload Aside Menu
     this.aside_items = [];
     this.aside_items.push(
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/',
+        styleClass: 'select-menu' + (this.router.url == '/' ? ' active' : ''),
+      },
       {
         label: 'Root Of Equations',
         icon: 'pi pi-circle-fill',
@@ -264,9 +218,6 @@ export class AppComponent implements OnInit {
         ],
       }
     );
-
-    // Disable Force Refresh
-    this.force_refresh = false;
 
     this._changeDetectorRef.detectChanges();
   }
